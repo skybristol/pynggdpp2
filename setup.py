57042ef98da33d7416adacfa7556c615a4573461
sbristol@usgs.gov <sbristol@usgs.gov>
2019-02-08 17:52:53 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pybggdpp',
+      version='0.0.1',
+      description='Bare bones version of pynggdpp',
+      url='http://github.com/nggdpp/pynggdpp2',
+      author='R. Sky Bristol',
+      author_email='sbristol@usgs.gov',
+      license='public-domain',
+      packages=['pynggdpp'],
+      zip_safe=False)
