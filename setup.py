a6830d90c2435b45e36869de66493d3e3265e461
sbristol@usgs.gov <sbristol@usgs.gov>
2019-02-21 06:51:45 -0700

    Got rid of geojson_utils for now and incremented version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pybggdpp',
-      version='0.0.3',
+      version='0.0.1',
       description='Bare bones version of pynggdpp',
       url='http://github.com/nggdpp/pynggdpp2',
       author='R. Sky Bristol',
